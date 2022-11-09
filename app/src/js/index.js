c17b6e67af607f7f6cb2b16ab300af3780714df0
MYOJT <EmpowerOjtCheckLists@gmail.com>
2022-11-09 23:57:38 +0900

    ちょい変更
    batがきどうしなくなってる

--- app/src/js/index.js
+++ app/src/js/index.js
@@ -1,7 +1,7 @@
-const app_js = require('app.js');
-const db_js = require('db.js');
-const login_js = require('login.js');
-const todo_js = require('todo.js');
+const app_js = require('./app.js');
+const db_js = require('./db.js');
+const login_js = require('./login.js');
+const todo_js = require('./todo.js');
 
 
 app_js.connect();
